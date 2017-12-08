03c2971232d4772f026d5d6b769c0ace4d08b3ff
SimonLyons <simon_lyons@hotmail.com>
2017-12-09 07:00:41 +1000

    Further work through baseball lessons.

--- The Analytics Edge - edX/R_Scripts/171208 - baseball analysis.R
+++ The Analytics Edge - edX/R_Scripts/171208 - baseball analysis.R
@@ -71,5 +71,67 @@ RunsAllowedReg$coefficients[1] + 0.297 * RunsAllowedReg$coefficients[2] + 0.370 
 ##################################################
 # Video 4: Using the Models to Make Predictions
 
+# Team On Base Percentage (OBP): 0.339
+# Team Slugging Percentage (SLG): 0.430
+
+team_runs_2002 <-  RunsReg$coefficients[1] + 0.339 * RunsReg$coefficients[2] + 0.430 *RunsReg$coefficients[3]
+team_runs_2002
+
+# Opposing Team On Base Percentage (OBP): 0.307
+# Opposing Team Slugging Percentage (SLG): 0.373
+opposing_team_runs_2002 <- RunsAllowedReg$coefficients[1] + 0.307 * RunsAllowedReg$coefficients[2] + 0.373 * RunsAllowedReg$coefficients[3]
+opposing_team_runs_2002
+
+RD <- team_runs_2002 - opposing_team_runs_2002
+Q_Wins <- WinsReg$coefficients[1] + WinsReg$coefficients[2] * RD
+
+##################################################
+# Quick Question
+# You have a budget of $1,500,000, and you have the choice between the following players:
+
+# First, forecast how many runs each player will score
+player_selection_data <- read.csv("player_seletion_Q.csv", header = TRUE)
+player_selection_data$RS <- RunsReg$coefficients[1] + player_selection_data$OBP * RunsReg$coefficients[2] + player_selection_data$SLG *RunsReg$coefficients[3]
+
+# Then determine dollars of salary spent per run scored
+player_selection_data$Salary_clean <- as.numeric(gsub(",", "", gsub("\\$", "", player_selection_data$Salary)))
+player_selection_data$value <- player_selection_data$Salary_clean / player_selection_data$RS
+
+# We see that Carlos Pena costs the least amount of salary per run scored, followed by Frank
+# Menechino. The two salaries together add up to only $595k. However, the highest number of 
+# runs inside the $1.5M spend cap can be achieved by combining Carlos Pena ($0.3M) and Jeremy
+# Giambi ($1.065M)
+
+# I'm not looking for my own method for automatically calculating the combinations
+# of salary and runs scored. This would be useful if the list of players was extremely
+# long, instead of just being five players.
+salary_RS <- function(Salary1, Salary2){
+  combined_salary <- Salary1 + Salary2
+  # combined_runs <- RS1 + RS2
+}
+
+unlist(lapply(player_selection_data$Salary_clean, sum, player_selection_data$Salary_clean))
+
+combn(player_selection_data$Player.Name, 2, FUN = paste)
+combn(player_selection_data$Salary_clean, 2, FUN = sum)
+combn(player_selection_data$RS, 2, FUN = sum)
+
+players_comb <- expand.grid(player_selection_data$Player.Name, player_selection_data$Player.Name)
+colnames(players_comb) <- c("Player_1", "Player_2")
+salary_comb <- expand.grid(player_selection_data$Salary_clean, player_selection_data$Salary_clean)
+colnames(salary_comb) <- c("Salary_1", "Salary_2")
+rs_comb <- expand.grid(player_selection_data$RS, player_selection_data$RS)
+colnames(rs_comb) <- c("rs_1", "rs_2")
+
+anal_comb <- cbind(players_comb, salary_comb, rs_comb)
+View(anal_comb)
+
+# Next step - calculate the combined salaries and runs scored and then start ordering and filtering.
+
+##################################################
+# Quick Question - Playoffs
+# https://courses.edx.org/courses/course-v1:MITx+15.071x+2T2017/courseware/f8d71d64418146f18a066d7f0379678c/6324edb8a22c4e35b937490647bfe203/?child=first
+
+teamRank = c(1,2,3,3,4,4,4,4,5,5)
 
 
